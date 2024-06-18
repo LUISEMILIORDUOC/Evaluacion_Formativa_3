7775718e6d54bd873a854522ceacffed2d168c8f
LUISEMILIORDUOC <166870458+LUISEMILIORDUOC@users.noreply.github.com>
2024-06-18 01:03:44 +0000

    modificado,escribi en el diccionario .

--- diccionario-lista.py
+++ diccionario-lista.py
@@ -1,6 +1,3 @@
 #lista en blanco para agregar empleados y cargos.
 empleados = []
 cargos = ["CEO", "Desarrollador", "Analista de datos"] # Cargos
-while cargo not in cargos:
-        print("Cargo no válido. Los cargos válidos son:", cargos)
-        cargo = input("Cargo (CEO, Desarrollador, Analista de datos): ")
