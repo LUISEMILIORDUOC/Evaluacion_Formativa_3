1fb93700e68773e8792d0323ff34a9392cfabb91
LUISEMILIORDUOC <166870458+LUISEMILIORDUOC@users.noreply.github.com>
2024-06-18 01:06:38 +0000

    paso a pasito

--- registro-de-trabajadores.py
+++ registro-de-trabajadores.py
@@ -3,3 +3,10 @@ def registrar_trabajador():
     nombre=input("Nombre: ")
     apellido=input("Apellido: ")
     cargo=input("Cargo (CEO, Desarrollador, Analista de datos): ")
+    while cargo not in cargo:
+        print("Cargo no válido. Los cargos válidos son:", cargo)
+        cargo = input("Cargo (CEO, Desarrollador, Analista de datos): ")
+    sueldo_bruto = float(input("Sueldo Bruto: "))
+    desc_salud = sueldo_bruto * 0.07
+    desc_afp = sueldo_bruto * 0.12
+    liquido_pagar = sueldo_bruto - desc_salud - desc_afp
