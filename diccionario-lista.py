7365417c10bc477692ab09d2bd905122219c2a9d
LUISEMILIORDUOC <166870458+LUISEMILIORDUOC@users.noreply.github.com>
2024-06-17 20:57:31 -0400

    Add files via upload

---
+++ diccionario-lista.py
@@ -0,0 +1,3 @@
+#lista en blanco para agregar empleados y cargos.
+empleados = []
+cargos = ["CEO", "Desarrollador", "Analista de datos"] # Cargos
---
+++ registro-de-trabajadores.py
@@ -0,0 +1,5 @@
+def registrar_trabajador():
+    print("\nRegistro de Trabajador")
+    nombre=input("Nombre: ")
+    apellido=input("Apellido: ")
+    cargo=input("Cargo (CEO, Desarrollador, Analista de datos): ")
