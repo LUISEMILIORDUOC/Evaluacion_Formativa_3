fb5ea5ba8238af92868a2d72ea138193e3989623
LUISEMILIORDUOC <166870458+LUISEMILIORDUOC@users.noreply.github.com>
2024-06-18 01:09:34 +0000

    se agrega diccionario de plantilla que se ocupara.

--- registro-de-trabajadores.py
+++ registro-de-trabajadores.py
@@ -10,3 +10,13 @@ def registrar_trabajador():
     desc_salud = sueldo_bruto * 0.07
     desc_afp = sueldo_bruto * 0.12
     liquido_pagar = sueldo_bruto - desc_salud - desc_afp
+#diccionario de plantilla de empleados
+    empleado = {
+        "Nombre": nombre,
+        "Apellido": apellido,
+        "Cargo": cargo,
+        "Sueldo Bruto": sueldo_bruto,
+        "Desc. Salud": desc_salud,
+        "Desc. AFP": desc_afp,
+        "Líquido a pagar": liquido_pagar
+    }
